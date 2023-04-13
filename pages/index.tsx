b92a710f8c9ce5337b0b745b3ca3ec8a7548d21a
Siddharth Sharma <siddharth.sharma1995@hotmail.com>
2023-04-14 02:58:28 +0530

    NextJs project created

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to my Next.js App!</h1>
+      <p>This is a basic example of a Next.js app.</p>
+    </div>
+  );
+};
+
+export default HomePage;
+
