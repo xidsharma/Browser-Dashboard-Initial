5257446e9dee9febaaed6a93d2e3eeedf6d2440b
Siddharth Sharma <siddharth.sharma1995@hotmail.com>
2023-05-08 00:14:30 +0530

    api keys removed

--- remote/api-constants.tsx
+++ remote/api-constants.tsx
@@ -1,10 +1,10 @@
-export const IP_INFO_URL = 'https://ipinfo.io/json?token=559005096c0064'
+export const IP_INFO_URL = 'https://ipinfo.io/json?token='
 
 export const QUOTES_URL = `https://api.quotable.io/quotes/random?maxLength=120`;
 
-// temp mail weather key = '20b08cc46af45097e33dab00b05372c7', '5945932e3e073a4a71a9e0939e950fdd'
+// temp mail weather key = '', ''
 
-const OPEN_WEATHER_MAP_API_KEY = '20b08cc46af45097e33dab00b05372c7';
+const OPEN_WEATHER_MAP_API_KEY = '';
 
 const PLACES_API_LIMIT = 5;
 export const PLACES_URL = (city: string, country: string) => `http://api.openweathermap.org/geo/1.0/direct?q=${city},,${country}&limit=${PLACES_API_LIMIT}&appid=${OPEN_WEATHER_MAP_API_KEY}`;
